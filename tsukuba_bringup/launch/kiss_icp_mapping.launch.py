f53b9626c4eb3b9e6dec062dc032413859d31a8f
K-yama-20 <k.yamanaka5342@gmail.com>
2026-08-12 14:57:28 +0900

    Integrate KISS-ICP launch into mapping bringup

--- tsukuba_bringup/launch/kiss_icp_mapping.launch.py
+++ tsukuba_bringup/launch/kiss_icp_mapping.launch.py
@@ -1,5 +1,29 @@
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from ament_index_python.packages import get_package_share_directory
+
+import os
 
 
 def generate_launch_description():
-	return LaunchDescription([])
+
+	kiss_icp_share = get_package_share_directory("kiss_icp")
+	kiss_icp_launch = os.path.join(
+		kiss_icp_share,
+		"launch",
+		"odometry.launch.py"
+	)
+
+	kiss_icp = IncludeLaunchDescription(
+		PythonLaunchDescriptionSource(kiss_icp_launch),
+		launch_arguments={
+			"topic": "/points",
+			"visualize": "true",
+		}.items(),
+	)
+
+	return LaunchDescription([
+	kiss_icp
+])
